5edf030eaa47119a7f0b12002366934feb3e2478
Atharva261297 <ath.bapat@gmail.com>
2020-03-13 17:46:20 +0530

    @EnableTransactionManagement added for services

--- src/main/java/com/atharva/auth/adminservice/service/ProjectService.java
+++ src/main/java/com/atharva/auth/adminservice/service/ProjectService.java
@@ -6,11 +6,13 @@ import com.atharva.auth.adminservice.model.ProjectModel;
 import com.atharva.auth.adminservice.utils.constants.ErrorCodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Base64;
 import java.util.UUID;
 
 @Service
+@EnableTransactionManagement
 public class ProjectService {
 
     @Autowired
